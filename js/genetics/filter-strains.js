9d5cebeae1101aac4dc65f70fe1c3f15823bee64
Tori <victoria.j.art@gmail.com>
2021-02-02 13:53:37 -0700

    added card filter functionality
    works by checking a hidden field that contains the json object info and matching that to the filter select

--- js/genetics/filter-strains.js
+++ js/genetics/filter-strains.js
@@ -1,13 +1,88 @@
 //listen for changes to the filter list
-    //apply changes immediately
+    //apply upon change
 
 function createEventListeners () {
-    console.log("filter!");
+    //listen for changes in the filter menu
+        //run filter 
+    let filterMenu = document.getElementById("genetics-filter-select");
+    if (filterMenu.addEventListener) {
+        filterMenu.addEventListener("change", (x) => {filterCards(x);}, false);
+    } else if (filterMenu.attachEvent){
+        filterMenu.attachEvent("onchange", (x) => {filterCards(x);});
+    }
+}
+
+function filterCards(x) {
+    let selectedFilterOption = x.target.value;
+    let filterOptions = [
+        "none",
+        "indica",
+        "sativa",
+        "indoors",
+        "outdoors",
+        "available"
+    ];
+    let geneticsCards = document.getElementsByClassName("genetics-card");
+    let cardsToFilter = [];
+
+    //check cardValue against selectedFilterOption
+        //if there is a match add 'false' to cardsToFilter (do not hide the card)
+        //if there is a match add 'true' to cardsToFilter (hide the card)
+    for(i=0; i < geneticsCards.length; i++) {
+        let hideCard;
+        let cardValue = geneticsCards[i].querySelector('input[type=hidden]');
+        cardValue = JSON.parse(cardValue.value);
+
+        if(selectedFilterOption === "indoors" || selectedFilterOption === "outdoors") {
+            for(grow = 0; grow < cardValue.grow.length; grow++) {
+                if(cardValue.grow[grow] === selectedFilterOption) {
+                    hideCard = false;
+                    break;
+                } else {
+                    hideCard = true;
+                }
+            }
+        } else if (selectedFilterOption === "available") {
+            if(cardValue.available) {
+                hideCard = false;
+            } else {
+                hideCard = true;
+            }
+        } else if (selectedFilterOption === "none") {
+            hideCard = false;
+        }else {
+            for(j in cardValue) {
+                if(cardValue[j] === selectedFilterOption) {
+                    hideCard = false;
+                    break; 
+                } else {
+                    hideCard = true;
+                }
+            }
+        }
+
+        //push true or false to cardsToFilter array
+        cardsToFilter.push(hideCard);
+    }
+
+    //hide/show cards based on cardsToFilter
+        //true === hide card
+        //false === show card
+    for(i=0; i < geneticsCards.length; i++) {
+        if(cardsToFilter[i]) {
+            geneticsCards[i].style.display = "none";
+        } else {
+            geneticsCards[i].style.display = "block";
+        }
+    }
+    
+
+    
 }
 
 //on pageload create eventlisteners 
 if (window.addEventListener) {
-    window.addEventListener("load", createEventLiseners, false);
+    window.addEventListener("load", createEventListeners, false);
 } else if (window.attachEvent) {
-    window.attachEvent("onload", createEventLiseners);
+    window.attachEvent("onload", createEventListeners);
 }
