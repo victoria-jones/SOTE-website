5af809b40f9fe37044d023992af1a0da72a225f5
belindanovoa <novoa.belinda@gmail.com>
2021-03-16 19:42:51 -0700

    genetics updated

--- js/genetics/strains-test.js
+++ js/genetics/strains-test.js
@@ -3,7 +3,7 @@ var strainList = [
         "name": "Cherry Punch",
         "type": ["sativa"],
         "grow": ["indoors", "outdoors"],
-        "description": "High Yield - ____ buds. This strain is ideal for those seeking relief from anxiety and stress.", 
+        "description": "8-9 wk strain. high Yield - Some Purple. Less “punch” nose than purple punch. This strain is ideal for those seeking relief from anxiety and stress.", 
         "photo": ["cherrypunch-tb.jpg", "cherry.jpg"],
         "available": false,
         "section": "featured"
@@ -12,8 +12,8 @@ var strainList = [
         "name": "Cereal Milk",
         "type": ["sativa", "indica"],
         "grow": ["indoors"],
-        "description": "Medium Yield - _______ buds. Euphoric, calming and energizing state. Soothing the body while enhancing creativity.",
-        "photo": ["cerealmilk-tb.jpg", "cerealmilk18.jpg"],
+        "description": " 9 wk strain. Medium high yield. Mostly purple. Pungent Gassy smell. Euphoric, calming and energizing state. Soothing the body while enhancing creativity.",
+        "photo": ["cerealmilk-tb.jpg", "cerealmilk28.jpg"],
         "available": true,
         "section": "featured"
     },
@@ -21,7 +21,7 @@ var strainList = [
         "name": "Purple Punch",
         "type": ["indica"],
         "grow": ["indoors"],
-        "description": "High Yield - Extreme Trichome Production - Helps patients with managing nausea, stress, minor body aches, and sleeplessness.",
+        "description": "7 wk strain. High Yield - Extreme Trichome Production - Helps patients with managing nausea, stress, minor body aches, and sleeplessness.",
         "photo": ["purplepunch-tb.jpg", "purplepunch4.jpg"],
         "available": true,
         "section": "featured"
@@ -30,7 +30,7 @@ var strainList = [
         "name": "Project X",
         "type": ["sativa", "indica"],
         "grow": ["indoors", "outdoors"],
-        "description": "High Yield but requires more deleafing, High Trichome Production - creative effects that are happy and uplifting",
+        "description": "9 wk strain. High Yield - Requires more deleafing, High Trichome Production - creative effects that are happy and uplifting",
         "photo": ["projectx-tb.jpg", "projectX52.jpg"],
         "available": false,
         "section": "featured"
@@ -39,7 +39,7 @@ var strainList = [
         "name": "Sunday Driver",
         "type": ["indica"],
         "grow": ["indoors, outdoors"],
-        "description": "Medium to High Yield - ____ Buds. Full relaxation. Euphoric effects followed by full-body relaxation.",
+        "description": "8-9 wk strain. High yield. Some purple. Funky smell. Full relaxation. Euphoric effects followed by full-body relaxation.",
         "photo": ["sundaydriver-tb.jpg", "sundaydriver2.jpg"],
         "available": true,
         "section": "featured"
@@ -48,7 +48,7 @@ var strainList = [
         "name": "Lava Cake",
         "type": ["indica"],
         "grow": ["indoors", "outdoors"],
-        "description": " Medium Yield - Dense Buds. Genetics hold relief for chronic pain, stress and anxiety.",
+        "description": " 8 wk strain. Medium Yield - Dense Buds. Genetics hold relief for chronic pain, stress and anxiety.",
         "photo": ["lavacake-tb.jpg", "lavacake3.jpg"],
         "available": true,
         "section": "featured"
